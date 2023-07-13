17c426383df1aaf865e31f3c3921ba3c1fdddee0
Raushan Kumar <raushan.development@gmail.com>
2023-07-13 18:31:48 +0530

    migrated to TS

---
+++ app/[movie]/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export async function generateStaticParams(){
+    const data = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+      )    
+    const res = await data.json()
+    return res.results.map((movie)=>({
+        movie: toString(movie.id)
+    }))
+}
+
+export default async function MovieDetails({params}){
+    // console.log(params);
+    const {movie}=params;
+    const imagePath = "https://image.tmdb.org/t/p/original";
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate: 60}});
+    const res = await data.json()
+    return (
+        <div>
+            <div>
+                <h2 className="text-3xl">{res.title}</h2>
+                <h2 className="text-2xl">{res.release_date}</h2>
+                <h2>Runtime: {res.runtime} minutes</h2>
+                <h2 className="my-2 bg-green-500 px-2 py-1 inline-block rounded-md">{res.status}</h2>
+            </div>
+            <div className="flex justify-center items-center">
+                <Image src={imagePath+res.backdrop_path} alt={res.title} height={1000} width={1000} 
+                priority
+                />
+            </div>
+            <div>
+                <h2>{res.overview}</h2>
+            </div>
+        </div>
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import './globals.css'
+import {Montserrat} from 'next/font/google'
+import Link from 'next/link'
+const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: '--font-montserrat',
+})
+
+export const metadata = {
+  title: 'Movie Info App',
+  description: 'A movie information app that utilises the powerful TMDB to cater movie information.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+    <body className={`${montserrat.className} mx-32 my-12`} style={{background: 'black', color: 'white'}}>
+        <nav >
+          <ul style={{display: 'flex', gap: '15px', justifyContent: 'center', alignItems: 'center'}}>
+            <Link href={'/'}>
+              <li>Popular</li>
+            </Link>
+            <Link href={'/upcoming'}>
+              <li>Upcoming</li>
+            </Link> 
+            <Link href={'/top_rated'}>
+              <li>Top Rated</li>
+            </Link>
+            
+          </ul>
+        </nav>
+        {children}
+      </body>
+    </html>
+  )
+}
